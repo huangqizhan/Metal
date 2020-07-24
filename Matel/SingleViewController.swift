dea1fa078be02ed78ef7fe57752060c4eac579eb
huangqizhan <>
2020-07-24 17:32:07 +0800

    brush

--- Matel/SingleViewController.swift
+++ Matel/SingleViewController.swift
@@ -14,11 +14,47 @@ class SingleViewController: UIViewController {
     @IBOutlet weak var canvas: MetalCanvas!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func segmengaction(_ sender: UISegmentedControl) {
+        do {
+            if sender.selectedSegmentIndex == 0 {
+                let pen = canvas.defaultBrush!
+                pen.name = "Pen"
+                pen.opacity = 0.1
+                pen.pointSize = 5
+                pen.pointStep = 0.5
+                pen.color = canvas.currentBrush.color
+                pen.use()
+            }else if (sender.selectedSegmentIndex == 1){
+                let pencil = try registerBrush(with: "pencil")
+                pencil.rotation = .random
+                pencil.pointSize = 3
+                pencil.pointStep = 2
+                pencil.forceSensitive = 0.3
+                pencil.opacity = 1
+                pencil.use()
+            }else if (sender.selectedSegmentIndex == 2){
+                let brush = try registerBrush(with: "brush")
+                brush.rotation = .ahead
+                brush.pointSize = 15
+                brush.pointStep = 2
+                brush.forceSensitive = 1
+                brush.color = canvas.currentBrush.color
+                brush.forceOnTap = 0.5
+                brush.use() 
+            }
+        } catch  {
+            print("");
+        }
 
-        // Do any additional setup after loading the view.
     }
     
     
+    private func registerBrush(with imageName: String) throws -> MetalBrush {
+        let texture = try canvas.makeTexture(with: UIImage(named: imageName)!.pngData()!)
+        return try canvas.registerBrush(name: imageName, textureId: texture.id);
+    }
     
     var r : CGFloat = 0
     var g : CGFloat = 0
